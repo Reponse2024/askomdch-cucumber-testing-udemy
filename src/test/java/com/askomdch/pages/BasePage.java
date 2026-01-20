76c0a1b79a1ac47cd4c3999cf95decb1a6d2a969
Reponse <reponse.iduha2023@kepler.org>
2026-01-20 11:27:21 +0200

    Removed comments

--- src/test/java/com/askomdch/pages/BasePage.java
+++ src/test/java/com/askomdch/pages/BasePage.java
@@ -2,14 +2,10 @@ package com.askomdch.pages;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.PageFactory;
-import org.openqa.selenium.support.ui.WebDriverWait;
-
-import java.time.Duration;
 
 public class BasePage {
     public BasePage (WebDriver driver) {
         this.driver = driver;
-        //wait = new WebDriverWait (driver, Duration.ofSeconds(15));
         PageFactory.initElements(driver, this);
     }
     public void load(String url){
